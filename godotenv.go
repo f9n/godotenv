3790428e083e4894f3d931a904276b49eafb98ec
pleycpl <h4rvey@protonmail.com>
2017-06-04 22:59:16 +0000

    updated EnvParser function, and adder parseLine function

--- godotenv.go
+++ godotenv.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+var GlobalEnv = make(map[string]string, 5)
+
 func ReadFile(path string) []string {
 	content, err := ioutil.ReadFile(path)
 	if err != nil {
@@ -16,14 +18,24 @@ func ReadFile(path string) []string {
 	return lines
 }
 
-func EnvParser(line string) {
-	r, err := regexp.Compile(`^(.+)=(.+)$`)
+func EnvParser(lines []string) {
+	for _, line := range lines {
+		err, key, value := parseLine(line)
+		if !err {
+			GlobalEnv[key] = value
+		}
+	}
+}
+
+func parseLine(line string) (bool, string, string) {
+	r, err := regexp.Compile(`^(\w+)=(.+)$`)
 	if err != nil {
 		fmt.Println(err)
 	}
 	result := r.FindStringSubmatch(line)
-	for k, v := range result {
-		fmt.Printf("%d. %s\n", k, v)
-	}
 	fmt.Println(result)
+	if len(result) > 0 {
+		return false, result[1], result[2]
+	}
+	return true, "", ""
 }
